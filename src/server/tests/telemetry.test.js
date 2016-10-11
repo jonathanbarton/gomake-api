de9012147d8d19cc1b24467c4e3d4aae528825f0
Jonathan Barton (MHE) <jonathan.barton@mheducation.com>
2016-10-11 17:14:13 -0400

    renames / moves files to new structure

--- src/server/tests/telemetry.test.js
+++ src/server/tests/telemetry.test.js
@@ -1,20 +1,16 @@
 import request from 'supertest-as-promised';
 import httpStatus from 'http-status';
 import chai from 'chai';
-import { expect } from 'chai';
 import app from '../../index';
 
 chai.config.includeStack = true;
 
 describe('#Telemetry ', () => {
-  describe('# GET /flight/:flightname', () => {
+  describe('# GET /flight/:flightname/telemetry', () => {
     it('should only accept a valid flightname in the URL', () => {
       request(app)
         .get('/flight/gomake-1/telemetry')
-        .expect(httpStatus.OK)
-        .then((res) => {
-          expect(res.body.content).to.be.an('array');
-        });
+        .expect(httpStatus.OK);
     });
   });
 });
