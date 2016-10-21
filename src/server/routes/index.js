24b8e8b973585113ec4a97189a11e88d482c4b2c
Neha Abrol <nehaabrol87@gmail.com>
2016-10-21 09:42:21 -0400

    add-auth0-jwt-check-all-endpoints

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -3,10 +3,20 @@ import userRoutes from './user';
 import authRoutes from './auth';
 import flightRosterRoutes from './flights';
 import flightRoutes from './flight';
+import config from '../../config/env';
+const jwt = require('express-jwt');
+
+const jwtCheck = jwt({
+  secret: new Buffer(config.jwtSecret, 'base64'),
+  audience: config.jwtAudience
+});
 
 const router = express.Router();	// eslint-disable-line new-cap
 
 /** GET /health-check - Check service health */
+
+router.use(jwtCheck);
+
 router.get('/health-check', (req, res) =>
   res.send('OK')
 );
