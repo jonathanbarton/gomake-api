ffc917f2294ece444422183d94db1ff18d8b2a76
Jonathan Barton (MHE) <jonathan.barton@mheducation.com>
2016-12-21 22:18:47 -0500

    adds flight history

--- src/server/routes/flight.js
+++ src/server/routes/flight.js
@@ -2,6 +2,7 @@ import express from 'express';
 import validate from 'express-validation';
 import telemetryCtrl from '../controllers/telemetry';
 import flightInfoCtrl from '../controllers/flight';
+import historyCtrl from '../controllers/history';
 import paramValidation from '../../config/param-validation';
 import config from '../../config/env';
 const jwt = require('express-jwt');
@@ -24,5 +25,9 @@ router.route('/:flightname/telemetry')
 /** POST flight telemetry */
 router.route('/:flightname/telemetry')
   .post(validate(paramValidation.telemetry), telemetryCtrl.postTelemetry);
+
+/** GET flight telemetry */
+router.route('/:flightname/history')
+  .get(validate(paramValidation.history), historyCtrl.getFlightHistory);
 
 export default router;
