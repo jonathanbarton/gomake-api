86285f901a6ff855c317862fce8bec1b454bba32
Neha Abrol <nehaabrol87@gmail.com>
2017-02-08 00:11:16 -0500

    add-user-authentication

--- src/server/middleware/authentication.js
+++ src/server/middleware/authentication.js
@@ -1,9 +1,5 @@
 'use strict';
 
-// import config from '../../config/env';
-// const jwt = require('jsonwebtoken');
-// const jwtSecret = config.jwtSecret;
-// const jwtAudience = config.jwtAudience;
 const AUTHENTICATION_FAILURE_STATUS = 401;
 const AUTHENTICATION_FAILURE_MESSAGE = 'Unauthorised';
 const request = require('request');
@@ -11,58 +7,37 @@ const request = require('request');
 function authentication(req, res, done) {
   const header = req.headers['authorization'];
   if (!header) {
-    return sendAuthenticationFailure(req, res);
+    return sendAuthenticationFailure(res);
   }
-  jwtVerify(req, res, header, done);
+  auth0Verify(req, res, header, done);
 }
 
-// function parseHeader(header) {
-//   return header.split(' ')[1];
-// }
 
-function jwtVerify(req, res, header) {
-  // const token = parseHeader(header);
-
-  var options = { method: 'GET',
-  url: 'https://gomake.auth0.com/api/v2/users',
-  headers: { authorization: `${header}` } };
-
-  console.log(header);
+function auth0Verify(req, res, header, done) {
+  var options = {
+    method: 'GET',
+    url: 'https://gomake.auth0.com/api/v2/users',
+    headers: {
+      authorization: `${header}`
+    }
+  };
 
   request(options, (error, response, body) => {
-    if (error) throw new Error(error);
-    console.log(body);
+    const pasrsedBody = JSON.parse(body);
+    const hasError = error || (pasrsedBody.error);
+
+    if (hasError) {
+      sendAuthenticationFailure(res);
+    } else {
+      req.user = pasrsedBody;
+      done();
+    }
   });
-
-  // const url = `https://gomake.auth0.com/authorize?response_type=token&client_id=KxwSnu3NgCtJKbtSXYc4gFtTOhgDn1rw`;
-  // request.get(url, (err, res, body) => {
-  //   if (err) {
-  //     console.log(err);
-  //   }
-  //   console.log(body);
-  //   if (res.statusCode !== 200) {
-  //     res.json({ body });
-  //     // console.log(body);
-  //     console.log('I am here');
-  //   }
-  // });
-
-  // jwt.verify(token, jwtSecret, { aud: jwtAudience }, (err, decoded) => {
-  //   if (err) {
-  //     sendAuthenticationFailure(req, res);
-  //   }
-  //   authenticate(req, decoded, done);
-  // });
 }
 
-function sendAuthenticationFailure(req, res) {
+function sendAuthenticationFailure(res) {
   res.status(AUTHENTICATION_FAILURE_STATUS);
   res.json({ message: AUTHENTICATION_FAILURE_MESSAGE });
 }
-
-// function authenticate(req, decoded, done) {
-//   req.user = decoded;
-//   done();
-// }
 
 module.exports = authentication;
