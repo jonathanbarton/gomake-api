3b649e34957579b1d9cc1b4c3ef8855489daf8d0
Jonathan Barton <jonathan.barton@mheducation.com>
2017-02-14 23:36:23 -0500

    removing Buffer from jwtSecret

--- src/config/env/docker.js
+++ src/config/env/docker.js
@@ -1,6 +1,6 @@
 export default {
   env: process.env.NODE_ENV,
-  jwtSecret: new Buffer(process.env.GM_JWT_SECRET, 'base64'),
+  jwtSecret: process.env.GM_JWT_SECRET,
   jwtAudience: process.env.GM_JWT_AUDIENCE,
   auth0Domain: process.env.GM_AUTH0_DOMAIN,
   auth0CallbackUrl: process.env.GM_AUTH0_CALLBACK_URL,
