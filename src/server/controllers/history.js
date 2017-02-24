72793dfeb34454de7e739d72f85e0c40f5a2d375
Jonathan Barton <jonathan.barton@mheducation.com>
2017-02-24 13:38:11 -0500

    fixes history endpoint

--- src/server/controllers/history.js
+++ src/server/controllers/history.js
@@ -7,23 +7,27 @@ const TELEMETRY_ERROR = 400;
 function getFlightHistory(req, res) {
   const flightName = req.params.flightname.toUpperCase();
   const getFlight = Flight.getFlightFromFlightName(flightName);
+  const startTransmitTime = req.query.start;
   getFlight
-    .then(getHistoryForAssignedDevices, sendFailureResponse(res))
+    .then(getHistoryForAssignedDevices(startTransmitTime), sendFailureResponse(res))
     .then(sendSuccessResponse(res), sendFailureResponse(res));
 }
 
-function getHistoryForAssignedDevices(foundFlight) {
-  const devices = foundFlight.deviceIds;
-  return Telemetry.find({ deviceId: { $in: devices } })
-    .sort('-transmitTime');
+function getHistoryForAssignedDevices(startTransmitTime) {
+  return (foundFlight) => {
+    const devices = foundFlight.deviceIds;
+    const startTimeAsDate = new Date(startTransmitTime);
+    return Telemetry.find({ deviceId: { $in: devices }, transmitTime: { $gte: startTimeAsDate } })
+      .sort('-transmitTime');
+  };
 }
 
 function sendFailureResponse(res) {
-  return (telemetry) => { res.json(contentResponse(telemetry)); };
+  return () => { res.sendStatus(TELEMETRY_ERROR); };
 }
 
 function sendSuccessResponse(res) {
-  return () => { res.sendStatus(TELEMETRY_ERROR); };
+  return (telemetry) => { res.json(contentResponse(telemetry)); };
 }
 
 export default {
